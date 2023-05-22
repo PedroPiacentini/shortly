12075707df18347a1494d45dda0fb6bd9b52dc24
Pedro <pedropiacentini@usp.br>
2023-05-22 15:14:43 -0300

    avaliador

--- src/database/database.connections.js
+++ src/database/database.connections.js
@@ -10,6 +10,5 @@ const configDatabase = {
 };
 
 if (process.env.MODE === "prod") configDatabase.ssl = true;
-configDatabase.ssl = true;
 
 export const db = new Pool(configDatabase);
--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
+import { urlSchema } from "../schemas/urls.Schema.js";
+import { authValidation } from "../middlewares/authValidation.middleware.js";
 
 const urlsRouter = Router();
+
+urlsRouter.post("/urls/shorten", validateSchema(urlSchema), authValidation,)
 
 export default urlsRouter;
