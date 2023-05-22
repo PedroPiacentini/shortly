805b92fdc3c365c869d919a5fa4806553ddc23c9
Pedro <pedropiacentini@usp.br>
2023-05-22 15:32:59 -0300

    feat: shorten

--- src/controllers/urls.controller.js
+++ src/controllers/urls.controller.js
@@ -1,1 +1,28 @@
 import { db } from "../database/database.connections.js";
+import { nanoid } from "nanoid";
+
+export async function shorten(req, res) {
+    const { url } = req.body;
+    const { user_id } = res.locals.session;
+
+    const shortUrl = nanoid(8);
+
+    try {
+        const hash = bcrypt.hashSync(password, 10)
+
+        const result = await db.query(`
+            INSERT INTO urls (user_id, short_url, original_url, visit_count)
+                VALUES ($1, $2, $3, $4)
+                RETURNING url_id;
+        `, [user_id, shortUrl, url, 0]);
+
+        res
+            .send({
+                id: result.rows[0].url_id,
+                shortUrl
+            })
+            .status(201);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -2,9 +2,10 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { urlSchema } from "../schemas/urls.Schema.js";
 import { authValidation } from "../middlewares/authValidation.middleware.js";
+import { shorten } from "../controllers/urls.controller.js";
 
 const urlsRouter = Router();
 
-urlsRouter.post("/urls/shorten", validateSchema(urlSchema), authValidation,)
+urlsRouter.post("/urls/shorten", validateSchema(urlSchema), authValidation, shorten);
 
 export default urlsRouter;
