93476fc4723700d65c47b5d1a9d57af99becd15e
Pedro <pedropiacentini@usp.br>
2023-05-22 14:02:50 -0300

    dump

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { userSchema, loginSchema } from "../schemas/auth.schemas.js";
+import { validateNewEmail } from "../middlewares/users.middlewares.js";
+
 
 const authRouter = Router();
 
-authRouter.post("/signup");
-authRouter.post("/signin");
+authRouter.post("/signup", validateSchema(userSchema), validateNewEmail, signUp);
+authRouter.post("/signin", validateSchema(loginSchema));
 
 export default authRouter;
