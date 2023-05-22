9149940824ec6c5cb98df3da17fe2237c765fa55
Pedro <pedropiacentini@usp.br>
2023-05-22 14:07:33 -0300

    signup

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { userSchema, loginSchema } from "../schemas/auth.schemas.js";
 import { validateNewEmail } from "../middlewares/users.middlewares.js";
+import { signUp } from "../controllers/auth.controller.js";
 
 
 const authRouter = Router();
