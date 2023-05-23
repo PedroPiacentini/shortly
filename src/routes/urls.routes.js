78c9d3d1d32181c74d828f526f6f236dae8385b9
Pedro <pedropiacentini@usp.br>
2023-05-23 13:30:50 -0300

    fix: routes

--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 import { urlSchema } from "../schemas/urls.Schema.js";
 import { authValidation } from "../middlewares/authValidation.middleware.js";
-import { getUrlById, shorten } from "../controllers/urls.controller.js";
+import { getUrlById, redirect, shorten } from "../controllers/urls.controller.js";
 
 const urlsRouter = Router();
 
 urlsRouter.post("/urls/shorten", validateSchema(urlSchema), authValidation, shorten);
 urlsRouter.get("/urls/:id", getUrlById);
-urlsRouter.get("/urls/open/:shortUrl", getUrlById);
+urlsRouter.get("/urls/open/:shortUrl", redirect);
+urlsRouter.delete("urls/:id", authValidation)
 
 export default urlsRouter;
