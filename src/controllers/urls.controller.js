e7006b66101428dc609f9bd6759547f1d9ab5afd
Pedro <pedropiacentini@usp.br>
2023-05-23 13:46:07 -0300

    feat: delete url

--- src/controllers/urls.controller.js
+++ src/controllers/urls.controller.js
@@ -34,11 +34,11 @@ export async function getUrlById(req, res) {
           `, [id]);
         if (url.rowCount === 0) return res.sendStatus(404);
 
-        res.send({
+        res.status(200).send({
             id,
             shortUrl: url.rows[0].short_url,
             url: url.rows[0].original_url
-        }).status(200);
+        });
 
     } catch (err) {
         res.status(500).send(err.message);
@@ -59,6 +59,26 @@ export async function redirect(req, res) {
 
         const originalUrl = url.rows[0].original_url;
         res.redirect(originalUrl);
+
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+export async function deleteUrlById(req, res) {
+    const { id } = req.params;
+    const { user_id } = res.locals.session;
+
+    try {
+        const url = await db.query(`
+        SELECT * FROM urls WHERE url_id = $1
+          `, [id]);
+        if (url.rowCount === 0) return res.sendStatus(404);
+        if (url.rows[0].user_id !== user_id) return res.sendStatus(401);
+        await db.query(`
+        DELETE FROM urls WHERE url_id = $1
+          `, [id]);
+        res.sendStatus(204);
 
     } catch (err) {
         res.status(500).send(err.message);
