cf148cf549ccda9d7cdef06cee10839e8d2467f7
Pedro <pedropiacentini@usp.br>
2023-05-23 15:44:51 -0300

    fix: get user me

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -9,7 +9,7 @@ export async function getUserMe(req, res) {
         SELECT
             users.user_id AS id,
             users.name,
-            SUM(urls.visit_count) AS 'visitCount',
+            SUM(urls.visit_count) AS "visitCount",
             json_agg(json_build_object(
             'id', urls.url_id,
             'shortUrl', urls.short_url,
@@ -32,7 +32,6 @@ export async function getUserMe(req, res) {
 }
 
 export async function getRanking(req, res) {
-    const { user_id } = res.locals.session;
 
     try {
 
